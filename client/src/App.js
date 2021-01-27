c30e10bb8389ba1294b897b65cb198064a6f18d5
dantedxwolf <teacher.martin@gmail.com>
2021-01-27 09:45:59 +0600

    Apollo Client setup done

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+
+const client = new ApolloClient({
+  uri:"http://localhost:4000/graphql"
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+       <div className="App">
+          This is apollo client
+        </div>
+    </ApolloProvider>
+   
+  );
+}
+
+export default App;
