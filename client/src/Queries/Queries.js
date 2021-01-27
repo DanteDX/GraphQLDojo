efd515266e95213e61b92210c310ad253b4bf55f
dantedxwolf <teacher.martin@gmail.com>
2021-01-27 17:09:21 +0600

    addBookMutation can work dynamically

--- client/src/Queries/Queries.js
+++ client/src/Queries/Queries.js
@@ -26,3 +26,13 @@ export const getAuthorsQuery = gql`
         }
     }
 `;
+
+export const addBookMutation = gql`
+    mutation($name:String!,$genre:String!,$authorId:ID!){
+        addBook(name:$name,genre:$genre,authorId:$authorId){
+            name
+            genre
+            authorId
+        }
+    }
+`;  
